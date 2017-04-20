3576ff92a3808c04708d1cff734017dc44cfad7f
Aria <574335078@qq.com>
2017-04-20 13:13:02 +0800

    fuck

---
+++ app/src/main/java/com/example/jewel/clothingrec/tagsview/TagsActivity.java
@@ -0,0 +1,20 @@
+package com.example.jewel.clothingrec.tagsview;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.example.jewel.clothingrec.R;
+
+/**
+ * Created by Aria on 2017/4/20.
+ */
+
+public class TagsActivity extends Activity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tag);
+    }
+}
